94bc4d1f35f2f8f81c790c0e17b1f86a61d44913
Catyka2 <catyka@mail.md>
2017-08-15 20:34:05 +0300

    Revert "rezolvare lectie 1"
    This reverts commit 92f1c2c1a3dd116e955980f56ffa389122245df6.

--- javascript/string/lectia1/script.js
+++ javascript/string/lectia1/script.js
@@ -7,8 +7,6 @@ var text = 'aa,bb,dd,f'//text
 var a = "text scris intro variabila"//text
 var b = "10"//text
 var c = 3//numar
-elemente = ''
- texthtml=''
 
 var v = text.split(',')//desparte textul in bucati dupa virgula
 console.log(v)
@@ -30,7 +28,7 @@ console.log(tt)
 
 console.log(text.search('aa'))//find gaseste un text in altul si intoarce positia primei aparitii
 console.log(text.search('dd'))
-console.log(text.search('bbb'))//apare -1 cand nu este in text
+console.log(text.search('bbb'))
 
 /*Exercitii
 1. afiseaza in divul #output textul din variabila text cu punct in loc de , in doua moduri
@@ -41,52 +39,4 @@ console.log(text.search('bbb'))//apare -1 cand nu este in text
 	c. daca este o cifra sa se adune cu 13 si sa se afiseza
 
 */
-$('#output').append(text.replace(/,/gi,'.'));//exer. 1
-for( var i=0;i<v.length; i++){//mai mic sau mai mare!!!
-	elemente+=v[i];
-	    if(i=>0){
-			elemente += '.';
-		}
-}
-$('#output').append('<br>',elemente);
-
-$('.lista').click(function(){ //exer. 2
-	var id = $(this).attr('id');
-	var nrid=parseInt(id)
-	cl(nrid+10)
-})
-
-$('#btn').click(function() { //exer.3
-    texthtml = $('#textare').val();	//preia textul din textbox
-		if(texthtml.search('catia')!=-1){ //punctul b
-			alert('este scris cuvintul catia'); 
-		}
-		var vectorFaraVirgula = texthtml.split(',')
-		console.log(vectorFaraVirgula)//a
-		console.log(vectorFaraVirgula.length - 1)//a
-		if(!isNaN(parseInt(texthtml)))//isNaN verifica sa aiba valoare
-			console.log(parseInt(texthtml)+13)//c
-		
-    	
-        	
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
