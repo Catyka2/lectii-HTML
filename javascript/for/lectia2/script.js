4e61ff2289f4bc2bdcac9c164d629fa4a49e8b52
Catyka2 <catyka@mail.md>
2017-08-16 20:12:26 +0300

    rezolvare lectie 3\javascript\for

--- javascript/for/lectia2/script.js
+++ javascript/for/lectia2/script.js
@@ -27,4 +27,37 @@ var vector2 = ['mar','cirese','rosii','abricoase', 'pere']
 6. cand apesi pe un element din prima lista sterge-l 
 
 */
+$('#ul2').append('<p>ex.1-6</p>');
+for(i=0; i<vector1.length; i++){ // ex 1
+	$('#ul2').append('<li>'+vector1[i]+'</li>')
+}
+$('#ul2').append('<p>ex.2-5</p>');
+for(j=0; j<vector2.length; j++){ // ex 2
+	$('#ul3').append('<li>'+vector2[j]+'</li>')
+}
+$('#output').append('<p>ex.3</p>');
+$('#ul1 li').each(function(){ // ex 3
+	if($(this).attr('id') == 'id-1'){
+		$('#output').append($(this).html());
+	}
+})
+
+$('#rezolvareButon').before('<p>ex.4</p>');
+$('#rezolvareButon').click(function(){ // ex 4
+	$(this).text('bine');
+})
+
+$('#ul3 li').each(function(index){ // ex 5
+	if(index%2==1){
+		$(this).css({'color':'red'});
+	}
+})
+
+$('#ul2 li').click(function(){ //ex 6
+	$(this).remove()
+})
+
+
+
+
 
--- javascript/for/lectia3/script.js
+++ javascript/for/lectia3/script.js
@@ -3,12 +3,16 @@
 var vector1 = [3,9,4,7,90]
 var vector2 = [1,3,5,7,8,9]
 var vector3 = []//elemente ce sunt in primul vector si nu sunt in al doilea
+var vector4 = [] //elemente ce sunt in ambii vectori
+var vector5 = [] //elemente ce sunt in al doilea vector dar nu sunt in primul
+var vector6 = []
 
 for(var i=0;i<vector1.length;i=i+1){//pentru fiecare indece din vector1
 	var ok = 1;//variabila care este 1 daca vector1[i] nu este in vector2, si 0 altfel
 	for(var j=0;j<vector2.length;j++){//pentru fiecare indice din vector2
 		if(vector1[i] == vector2[j]){
 			ok = 0; // am gasit numere egale
+			vector4.push(vector1[i]) // adauga eamentele ce sunt in ambii vectori Ex 1
 			break;// oprim for'ul cu j 
 		}
 	}
@@ -16,7 +20,7 @@ for(var i=0;i<vector1.length;i=i+1){//pentru fiecare indece din vector1
 		vector3.push(vector1[i])//adaugam elementul in vector3
 	}
 }
-
+$('#output').append('<p> ex 1: elemente ce sunt in ambii vectori:'+vector4+'</p>'); //Ex 1
 console.log(vector3)
 
 //schimbarea valorilor a doua variabile
@@ -39,3 +43,60 @@ console.log([a,b])
 
 */
 
+for(var j=0;j<vector2.length;j++){ //ex 2
+    var ok = 1;
+	for(var i=0;i<vector1.length;i=i+1){
+		if(vector2[j] == vector1[i]){
+			ok = 0;
+			break;	
+		}	
+	}
+	if(ok == 1){
+		vector5.push(vector2[j])
+	}
+}	
+$('#output').append('<p> ex 2: elemente ce sunt in al doilea vector si nu sunt in primul:'+vector5+'</p>');
+
+
+ var max = 0;
+indiceMaxim = 0;	
+for(var i=0;i<vector1.length;i=i+1){
+	if(vector1[i]>max){ //afla nr. maxim din vector1
+		max=vector1[i];
+		indiceMaxim=i;
+	}
+}
+console.log(vector1[indiceMaxim]);
+
+
+indiceMinim = 0;	
+ var min = 10000;
+for(var j=0;j<vector2.length;j=j+1){
+	if(vector2[j]<min){ // afla nr. minim din vector2
+		min = vector2[j]
+		indiceMinim=j;
+	}
+}
+console.log(vector2[indiceMinim]);
+
+console.log(vector1)
+vector1[indiceMaxim] = vector2[indiceMinim];//schimba nr. maxim din vector1 cu nr. minim din vector2
+console.log(vector1)
+$('#output').append('<p>ex3: vector schimbat</p>')//afiseaza vector1 cu nr maxim schimbat pe cel mai mic nr din vector2
+for(var jll=0;jll<vector1.length;jll=jll+1)
+	$('#output').append('<p>element nr:'+(jll+1)+' este '+vector1[jll]+'</p>');
+
+
+
+
+
+
+
+
+			
+				
+			
+			
+			
+			
+		
